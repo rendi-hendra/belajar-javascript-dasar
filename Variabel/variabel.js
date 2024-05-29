c156a36bdffc3514ee554046695d4338d672e62f
babe25 <babehndr5@gmail.com>
2024-05-30 01:19:31 +0700

    Variabel

---
+++ Variabel/variabel.js
@@ -0,0 +1,20 @@
+// Variabel dalam JavaScript adalah tempat untuk menyimpan data. Ada tiga cara mendeklarasikannya:
+
+// var: Variabel global atau lokal dalam fungsi. Bisa diubah.
+// let: Variabel dalam cakupan blok. Bisa diubah.
+// const: Variabel yang nilainya tetap. Harus diinisialisasi saat deklarasi.
+// Contoh:
+
+let nama = "Rendi";
+const umur = 19;
+
+console.log(nama);
+console.log(umur);
+
+nama = "Hendra";
+// umur = 20; Error karena variable umur constant(Tidak bisa di rubah)
+
+console.log(nama);
+console.log(umur);
+
+// Variabel menyimpan berbagai jenis data seperti angka, teks, objek, dan array.
